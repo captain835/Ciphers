369cbc71cf483b90a46cd5fdf12c8c3febe9ee85
captain835 <83428573+captain835@users.noreply.github.com>
2021-10-26 08:56:16 +0300

    Updated Transpositional_Cipher.cs

--- Transpositional_Cipher.cs
+++ Transpositional_Cipher.cs
@@ -38,6 +38,46 @@ namespace Transpositional_Cipher
                     }
                 }
             }
+            if (command == "decrypt")
+                Decrypt(input, key);
+            void Decrypt(string input, int key)
+            {
+                int rows = input.Length / key;
+                if (input.Length % key != 0)
+                    rows++;
+                char[,] matrix = new char[rows, key];
+                int x = 0, y = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (x == rows - 1)
+                    {
+                        if (y <= input.Length % key || input.Length % key == 0)
+                        {
+                            matrix[x, y] = input[i];
+                            x = 0;
+                            y++;
+                            continue;
+                        }
+                        else
+                        {
+                            x = 0;
+                            y++;
+                            i--;
+                            continue;
+                        }
+                    }
+                    else
+                        matrix[x, y] = input[i];
+                    x++;
+                }
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int i1 = 0; i1 < key; i1++)
+                    {
+                        Console.Write(matrix[i, i1]);
+                    }
+                }
+            }
         }
     }
 }
