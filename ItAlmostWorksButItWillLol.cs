fe4f0e87fbeccacf2417a4c1e44fb19621660e1c
captain835 <83428573+captain835@users.noreply.github.com>
2022-02-08 09:39:49 +0200

    AlmostWorks(Bruteforce)

--- ItAlmostWorksButItWillLol.cs
+++ ItAlmostWorksButItWillLol.cs
@@ -14,6 +14,7 @@ namespace Affine_Cipher
             string encryptedMessage = Encryption();
             Console.WriteLine(encryptedMessage);
             Console.WriteLine(Decryption(encryptedMessage));
+            BruteForce(encryptedMessage);
             string Encryption()
             {
                 string encryptedMessage = "";
@@ -41,7 +42,7 @@ namespace Affine_Cipher
                     }
                 return encryptedMessage;
             }
-            int KeyBCheck(int multiplct)
+            int KeyBCheck(int multiplct)//needs to be changed to be compatible with bruteforce
             {
                 int add = 0;
                 if ((multiplct + keyB) % 26 != 0)
@@ -50,7 +51,7 @@ namespace Affine_Cipher
                     return (multiplct + keyB) / 26 + add;
                 else return 0;
             }
-            int KeyACheck(int multiplct)
+            int KeyACheck(int multiplct)//needs to be changed to be compatible with bruteforce
             {
                 int counter = 0;
                 while (true)
@@ -87,6 +88,55 @@ namespace Affine_Cipher
                         }
                     }
                 return decryptedMessage;
+            }
+            void BruteForce(string enMessage)
+            {
+                int matches = 0;
+                string possibleMessage = "";
+                Console.WriteLine();
+                for (int i = message.Length+1; i < 100000; i++)
+                {
+                    Console.WriteLine($"Epoch: {i-message.Length}");
+                    //ClearCurrentConsoleLine();
+                    int keyA = i / message.Length;
+                    int keyB = i % message.Length;
+                    foreach (char chr in enMessage)
+                        for (int i1 = 0; i1 < alpha.Length; i1++)
+                        {
+                            if (chr == alpha[i1])
+                            {
+                                if (Char.IsUpper(chr))
+                                {
+                                    possibleMessage += Char.ToUpper(alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))]);
+                                    break;
+                                }
+                                else
+                                {
+                                    possibleMessage += alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))];
+                                    break;
+                                }
+                            }
+                            else if (chr == ' ')
+                            {
+                                possibleMessage += " ";
+                                break;
+                            }
+                        }
+                }
+                if (possibleMessage.ToLower().Contains(" a ") || possibleMessage.ToLower().Contains(" and ") || possibleMessage.ToLower().Contains(" an ") || possibleMessage.ToLower().Contains(" or ") || possibleMessage.ToLower().Contains(" is ") || possibleMessage.ToLower().Contains(" for ") || possibleMessage.ToLower().Contains(" but ") || possibleMessage.ToLower().Contains(" from ") || possibleMessage.ToLower().Contains(" he ") || possibleMessage.ToLower().Contains(" she ") || possibleMessage.ToLower().Contains(" it ") || possibleMessage.ToLower().Contains(" I ") || possibleMessage.ToLower().Contains(" you ") || possibleMessage.ToLower().Contains(" this "))
+                {
+                    Console.WriteLine(possibleMessage);
+                    matches++;
+                }
+                Console.WriteLine($"{matches} found.");
+            }
+
+            void ClearCurrentConsoleLine()
+            {
+                int currentLineCursor = Console.CursorTop;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLineCursor);
             }
         }
     }
