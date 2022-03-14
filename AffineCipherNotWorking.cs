6e3ec9fc85c381a362783e41659ae76b9a5f3387
captain835 <83428573+captain835@users.noreply.github.com>
2022-03-14 22:56:49 +0200

    Update AffineCipherNotWorking.cs

--- AffineCipherNotWorking.cs
+++ AffineCipherNotWorking.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Numerics;
 
 namespace Affine_Cipher
 {
@@ -14,7 +15,7 @@ namespace Affine_Cipher
             string encryptedMessage = Encryption();
             Console.WriteLine(encryptedMessage);
             Console.WriteLine(Decryption(encryptedMessage));
-            BruteForce(encryptedMessage);
+            //BruteForce(encryptedMessage);
             string Encryption()
             {
                 string encryptedMessage = "";
@@ -25,12 +26,12 @@ namespace Affine_Cipher
                         {
                             if (Char.IsUpper(chr))
                             {
-                                encryptedMessage += Char.ToUpper(alpha[(i * keyA % alpha.Length + keyB) % 26]);
+                                encryptedMessage += Char.ToUpper(alpha[((i * keyA) % message.Length + keyB) % 26]);
                                 break;
                             }
                             else
                             {
-                                encryptedMessage += alpha[(i * keyA % alpha.Length + keyB) % 26];
+                                encryptedMessage += alpha[((i * keyA) % message.Length + keyB) % 26];
                                 break;
                             }
                         }
@@ -42,28 +43,9 @@ namespace Affine_Cipher
                     }
                 return encryptedMessage;
             }
-            int KeyBCheck(int multiplct)//needs to be changed to be compatible with bruteforce
-            {
-                int add = 0;
-                if ((multiplct + keyB) % 26 != 0)
-                    add++;
-                if (multiplct + keyB > 26)
-                    return (multiplct + keyB) / 26 + add;
-                else return 0;
-            }
-            int KeyACheck(int multiplct)//needs to be changed to be compatible with bruteforce
-            {
-                int counter = 0;
-                while (true)
-                {
-                    if ((multiplct + 26 * counter) % keyA == 0)
-                        return (multiplct + 26 * counter) / keyA;
-                    counter++;
-                }
-
-            }
             string Decryption(string enMessage)
             {
+                // BigInteger.ModPow(a, n - 2, n)
                 string decryptedMessage = "";
                 foreach (char chr in enMessage)
                     for (int i = 0; i < alpha.Length; i++)
@@ -72,12 +54,12 @@ namespace Affine_Cipher
                         {
                             if (Char.IsUpper(chr))
                             {
-                                decryptedMessage += Char.ToUpper(alpha[KeyACheck(i - keyB % 26 + KeyBCheck(i - keyB % 26))]);
+                                decryptedMessage += Char.ToUpper(alpha[(int)BigInteger.ModPow(keyA, message.Length - 2, message.Length)*(i - keyB)%26]);
                                 break;
                             }
                             else
                             {
-                                decryptedMessage += alpha[KeyACheck(i - keyB % 26 + KeyBCheck(i - keyB % 26))];
+                                decryptedMessage += alpha[(int)BigInteger.ModPow(keyA, message.Length - 2, message.Length) * (i - keyB) % 26];
                                 break;
                             }
                         }
@@ -89,55 +71,55 @@ namespace Affine_Cipher
                     }
                 return decryptedMessage;
             }
-            void BruteForce(string enMessage)
-            {
-                int matches = 0;
-                string possibleMessage = "";
-                Console.WriteLine();
-                for (int i = message.Length+1; i < 100000; i++)
-                {
-                    Console.WriteLine($"Epoch: {i-message.Length}");
-                    //ClearCurrentConsoleLine();
-                    int keyA = i / message.Length;
-                    int keyB = i % message.Length;
-                    foreach (char chr in enMessage)
-                        for (int i1 = 0; i1 < alpha.Length; i1++)
-                        {
-                            if (chr == alpha[i1])
-                            {
-                                if (Char.IsUpper(chr))
-                                {
-                                    possibleMessage += Char.ToUpper(alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))]);
-                                    break;
-                                }
-                                else
-                                {
-                                    possibleMessage += alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))];
-                                    break;
-                                }
-                            }
-                            else if (chr == ' ')
-                            {
-                                possibleMessage += " ";
-                                break;
-                            }
-                        }
-                }
-                if (possibleMessage.ToLower().Contains(" a ") || possibleMessage.ToLower().Contains(" and ") || possibleMessage.ToLower().Contains(" an ") || possibleMessage.ToLower().Contains(" or ") || possibleMessage.ToLower().Contains(" is ") || possibleMessage.ToLower().Contains(" for ") || possibleMessage.ToLower().Contains(" but ") || possibleMessage.ToLower().Contains(" from ") || possibleMessage.ToLower().Contains(" he ") || possibleMessage.ToLower().Contains(" she ") || possibleMessage.ToLower().Contains(" it ") || possibleMessage.ToLower().Contains(" I ") || possibleMessage.ToLower().Contains(" you ") || possibleMessage.ToLower().Contains(" this "))
-                {
-                    Console.WriteLine(possibleMessage);
-                    matches++;
-                }
-                Console.WriteLine($"{matches} found.");
-            }
+            //void BruteForce(string enMessage)
+            //{
+            //    int matches = 0;
+            //    string possibleMessage = "";
+            //    Console.WriteLine();
+            //    for (int i = message.Length + 1; i < 100000; i++)
+            //    {
+            //        Console.WriteLine($"Epoch: {i - message.Length}");
+            //        //ClearCurrentConsoleLine();
+            //        int keyA = i / message.Length;
+            //        int keyB = i % message.Length;
+            //        foreach (char chr in enMessage)
+            //            for (int i1 = 0; i1 < alpha.Length; i1++)
+            //            {
+            //                if (chr == alpha[i1])
+            //                {
+            //                    if (Char.IsUpper(chr))
+            //                    {
+            //                        possibleMessage += Char.ToUpper(alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))]);
+            //                        break;
+            //                    }
+            //                    else
+            //                    {
+            //                        possibleMessage += alpha[KeyACheck(i1 - keyB % 26 + KeyBCheck(i1 - keyB % 26))];
+            //                        break;
+            //                    }
+            //                }
+            //                else if (chr == ' ')
+            //                {
+            //                    possibleMessage += " ";
+            //                    break;
+            //                }
+            //            }
+            //    }
+            //    if (possibleMessage.ToLower().Contains(" a ") || possibleMessage.ToLower().Contains(" and ") || possibleMessage.ToLower().Contains(" an ") || possibleMessage.ToLower().Contains(" or ") || possibleMessage.ToLower().Contains(" is ") || possibleMessage.ToLower().Contains(" for ") || possibleMessage.ToLower().Contains(" but ") || possibleMessage.ToLower().Contains(" from ") || possibleMessage.ToLower().Contains(" he ") || possibleMessage.ToLower().Contains(" she ") || possibleMessage.ToLower().Contains(" it ") || possibleMessage.ToLower().Contains(" I ") || possibleMessage.ToLower().Contains(" you ") || possibleMessage.ToLower().Contains(" this "))
+            //    {
+            //        Console.WriteLine(possibleMessage);
+            //        matches++;
+            //    }
+            //    Console.WriteLine($"{matches} found.");
+            //}
 
-            void ClearCurrentConsoleLine()
-            {
-                int currentLineCursor = Console.CursorTop;
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, currentLineCursor);
-            }
+            //void ClearCurrentConsoleLine()
+            //{
+            //    int currentLineCursor = Console.CursorTop;
+            //    Console.SetCursorPosition(0, Console.CursorTop);
+            //    Console.Write(new string(' ', Console.WindowWidth));
+            //    Console.SetCursorPosition(0, currentLineCursor);
+            //}
         }
     }
 }
